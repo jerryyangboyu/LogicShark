f13ef82b421c3e4b560b1a23ebd0fabf5ca96baf
sunmyjason <69485885+sunmyjason@users.noreply.github.com>
2022-03-15 15:53:56 +0000

    Update file with tests

--- src/algorithm.py
+++ src/algorithm.py
@@ -44,4 +44,16 @@ def string_replacement(temp):
     result = temp2.replace('′', 'not ')
     return result
 
-
+# test
+# print("1 : ", cnf('A ^ B | C'))
+# print("2 : ", dnf('A ^ B | C'))
+# print("3 : ", simplify('A ^ B | C'))
+# print("4 : ", cnf('A ^ (A | C) | (C | ~B)'))
+# print("5 : ", simplify('A ^ (A | C) | (C | ~B)'))
+# print('6', cnf('((~A ^ B) | (C | D)) | ((D & ~C) | (~A | ~B))'))
+# print('7', dnf('((~A ^ B) | (C | D)) | ((D & ~C) | (~A | ~B))'))
+# print('8', simplify('((~A ^ B) | (C | D)) | ((D & ~C) | (~A | ~B))'))
+# print('9', dnf('A ^ ~B | (B ^ (~C | A))'))
+# print('10', cnf('A ^ ~B | (B ^ (~C | A))'))
+# print('11', simplify('A ^ ~B | (B ^ (~C | A))'))
+# print('12', cnf('W | ((X | ~Y) ^ (Z ^ Y))'))
