149b59b1e4a42d6af940e98f4f154615d7a337b2
Muyang Sun <69485885+sunmyjason@users.noreply.github.com>
2022-03-15 21:17:07 +0000

    Jason: Added event handling for each button

--- src/ToolBar.py
+++ src/ToolBar.py
@@ -4,6 +4,8 @@ from PySide6.QtCore import Qt
 from PySide6.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QFrame, QToolButton, QLabel, QPushButton, \
     QSizePolicy, QGroupBox
 
+from algorithm import cnf, dnf, simplify
+
 
 class ToolBar(QFrame):
     def __init__(self):
@@ -28,30 +30,82 @@ class ToolBar(QFrame):
         self.ToolbarLayout.addWidget(self.FunctionButtonGroup)
         self.setLayout(self.ToolbarLayout)
 
+
     @staticmethod
     def createGraphButtonGroup():
         GroupBox = QGroupBox()
         GroupBox.setTitle("Graph")
         GroupBoxLayout = QHBoxLayout()
-        GroupBoxLayout.addWidget(QPushButton("NEW"))
-        GroupBoxLayout.addWidget(QPushButton("OPEN"))
-        GroupBoxLayout.addWidget(QPushButton("SAVE"))
-        GroupBoxLayout.addWidget(QPushButton("EXPORT"))
+        button1 = QPushButton('NEW')
+        button2 = QPushButton('OPEN')
+        button3 = QPushButton('SAVE')
+        button4 = QPushButton('EXPORT')
+        GroupBoxLayout.addWidget(button1)
+        GroupBoxLayout.addWidget(button2)
+        GroupBoxLayout.addWidget(button3)
+        GroupBoxLayout.addWidget(button4)
         GroupBox.setLayout(GroupBoxLayout)
+
+        # Button signal handling
+        button1.clicked.connect(ToolBar.clickedNewButton)
+        button2.clicked.connect(ToolBar.clickedOpenButton)
+        button3.clicked.connect(ToolBar.clickedSaveButton)
+        button4.clicked.connect(ToolBar.clickedExportButton)
         return GroupBox
+
 
     @staticmethod
     def createFunctionButtonGroup():
         GroupBox = QGroupBox()
         GroupBox.setTitle("Functions")
         GroupBoxLayout = QHBoxLayout()
-        GroupBoxLayout.addWidget(QPushButton("CNF"))
-        GroupBoxLayout.addWidget(QPushButton("DNF"))
-        GroupBoxLayout.addWidget(QPushButton("Simplify"))
-        GroupBoxLayout.addWidget(QPushButton("Custom"))
-        GroupBoxLayout.addWidget(QPushButton("Clear"))
+        button5 = QPushButton("CNF")
+        button6 = QPushButton("DNF")
+        button7 = QPushButton("Simplify")
+        button8 = QPushButton("Custom")
+        button9 = QPushButton("Clear")
+        GroupBoxLayout.addWidget(button5)
+        GroupBoxLayout.addWidget(button6)
+        GroupBoxLayout.addWidget(button7)
+        GroupBoxLayout.addWidget(button8)
+        GroupBoxLayout.addWidget(button9)
         GroupBox.setLayout(GroupBoxLayout)
+
+        # Button signal handling
+        button5.clicked.connect(ToolBar.clickedCNFButton)
+        button6.clicked.connect(ToolBar.clickedDNFButton)
+        button7.clicked.connect(ToolBar.clickedSimplifyButton)
+        button8.clicked.connect(ToolBar.clickedCustomButton)
+        button9.clicked.connect(ToolBar.clickedClearButton)
         return GroupBox
+
+
+    def clickedNewButton(self):
+        print("NEW")
+
+    def clickedOpenButton(self):
+        print("OPEN")
+
+    def clickedSaveButton(self):
+        print("SAVE")
+
+    def clickedExportButton(self):
+        print("EXPORT")
+
+    def clickedCNFButton(self):
+        print('CNF: ')
+
+    def clickedDNFButton(self):
+        print('DNF: ')
+
+    def clickedSimplifyButton(self):
+        print('Simplify: ')
+
+    def clickedCustomButton(self):
+        print("CUSTOM")
+
+    def clickedClearButton(self):
+        print('CLEAR')
 
     def createLogoWidget(self):
         logoWidget = QLabel()
@@ -63,4 +117,6 @@ class ToolBar(QFrame):
 
         logoWidget.setPixmap(pixelMap)
         return logoWidget
+
+
 
